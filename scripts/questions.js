f5570a4e92f725fa90afa29389aa6ab5a205355f
Hamza <mhamza2551@gmail.com>
2020-07-31 14:16:51 +0500

    Create questions.js

---
+++ scripts/questions.js
@@ -0,0 +1,65 @@
+var questions = [{
+        question: 'The term ‘Computer’ is derived from__________?',
+        answer: 'Latin',
+        options: [
+            'Latin', 'German', 'French', 'English'
+        ]
+
+    },
+    {
+        question: 'Who is the father of Computer?',
+        answer: 'Charles Babbage',
+        options: [
+            'Allen Turing', 'Charles Babbage', 'Simur Cray', 'Augusta Adaming'
+        ]
+
+    },
+    {
+        question: 'The basic operations performed by a computer are__________?',
+        answer: 'All the above',
+        options: [
+            'Arithmetic operation', 'Logical operation', 'Storage and relative', 'All the above'
+        ]
+
+    },
+    {
+        question: 'Who is the father of Internet ?',
+        answer: 'Vint Cerf',
+        options: [
+            'Chares Babbage', 'Vint Cerf', 'Denis Riche', 'Martin Cooper'
+        ]
+
+    },
+    {
+        question: 'If a computer has more than one processor then it is known as__________?',
+        answer: 'Multiprocessor',
+        options: [
+            'Uni-process', 'Multiprocessor', 'Multi-threaded', 'Multi-programming'
+        ]
+
+    },
+    {
+        question: 'A light sensitive device that converts drawing, printed text or other images into digital form is___________?',
+        answer: 'Scanner',
+        options: [
+            'Keyboard', 'Scanner', 'OMR', 'None of these'
+        ]
+
+    },
+    {
+        question: 'WWW stands for___________?',
+        answer: 'World Wide Web',
+        options: [
+            'World Whole Web', 'Wide World Web', 'Wide World Web', 'World Wide Web'
+        ]
+
+    },
+    {
+        question: 'A collection of system programs that controls and co-ordinates the overall operations of a computer system is called____________?',
+        answer: 'Operating system',
+        options: [
+            'System software', 'Operating system', 'Utility program', 'Device driver'
+        ]
+
+    }
+]
